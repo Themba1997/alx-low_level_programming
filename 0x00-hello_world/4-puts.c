560c5291bcb99f43884bf5ef842e5354c889c3f4
Themba1997 <mphondlaze@gmail.com>
2022-06-05 14:11:55 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+
 /**
- *Decription : Write a C program that prints exactly.
- *Main-Entry point
- *Return 0 - Always 0 (Success)
-*/
+ *Decription - Write a C program that prints exactly.
+ *
+ *Main - main entry point.
+ *
+ *Return: Always return value 0 (Success)
+ *
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
